93fffc862441db4d73a446823a0a5e5dd3d42601
mdhodges <megandiane@Megans-MacBook-Air.local>
2017-11-23 09:45:08 -0600

    updated my directory.

--- Code/MSDS6306_CaseStudy_2.R
+++ Code/MSDS6306_CaseStudy_2.R
@@ -14,7 +14,7 @@ sessionInfo()
 BaseDir <- "C:/Vivek/Data_Science/MSDS6306-DoingDataScience/Case_Study_2/MSDS6306_CaseStudy_2/"
 
 # Executing from Megan's System:
-BaseDir <- "/Users/megandiane/Desktop/DDS_Class/Case_Study_2"
+BaseDir <- "/Users/megandiane/Desktop/DDS_Class/Case_Study_2/MSDS6306_CaseStudy_2"
 
 # Set Other Working Directories and File Path
 DataDir <- paste(BaseDir,"Data", sep = "/")
