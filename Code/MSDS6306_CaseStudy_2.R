4f95664119bbef9ba3606599b082b34d2a4abb24
vviswanathan <31835297+vviswanathan@users.noreply.github.com>
2017-11-28 19:18:52 -0600

    Update MSDS6306_CaseStudy_2.R

--- Code/MSDS6306_CaseStudy_2.R
+++ Code/MSDS6306_CaseStudy_2.R
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-# Load Libraries
-library(repmis)
-library(dplyr)
-library(tidyverse)
-library(tidyr)
-library(ggplot2)
-
-# R - environment
-sessionInfo()
-
-# Set Base Directory:
-
-# Executing from Vivek's System:
-BaseDir <- "C:/Vivek/Data_Science/MSDS6306-DoingDataScience/Case_Study_2/MSDS6306_CaseStudy_2/"
-
-# Executing from Megan's System:
-BaseDir <- "/Users/megandiane/Desktop/DDS_Class/Case_Study_2/MSDS6306_CaseStudy_2"
-
-# Set Other Working Directories and File Path
-DataDir <- paste(BaseDir,"Data", sep = "/")
-CodeDir <- paste(BaseDir,"Code", sep = "/")
-PresenatationDir <- paste(BaseDir,"Presentation", sep = "/")
-
-ProcrastinationDataFile <- paste(DataDir, "Procrastination.csv", sep = "/")
-
-# Read the Data File
-ProcrastinationData <- read.csv(ProcrastinationDataFile, sep = ",", header = T)
-=======
 #install packages required
 if (!require(tidyverse)) install.packages("tidyverse")
 if (!require(repmis)) install.packages("repmis")
@@ -125,4 +96,3 @@ LowHumDev <- HumDevUrl %>%
   html_table(fill = T)
 
 LowHumDev <- data.frame(LowHumDev[[1]])
->>>>>>> a6db00f2e055450147bd69ce9f348ceefab390fb
